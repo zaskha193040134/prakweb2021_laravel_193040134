48db9f8f3ad339caf5b813cc78d2e21a5a331cb8
zaskha193040134 <193040134.zaskha@mail.unpas.ac.id>
2021-11-15 12:11:20 +0700

    22. Menambah Fitur Preview, Update & Delete untuk Image

--- coba-laravel/app/Http/Controllers/DashboardPostController.php
+++ coba-laravel/app/Http/Controllers/DashboardPostController.php
@@ -7,6 +7,7 @@ use App\Models\Category;
 use Illuminate\Http\Request;
 use \Cviebrock\EloquentSluggable\Services\SlugService;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Storage;
 
 class DashboardPostController extends Controller
 {
@@ -101,6 +102,7 @@ class DashboardPostController extends Controller
         $rules = [
             'title' => 'required|max:255',
             'category_id' => 'required',
+            'image' => 'image|file|max:5120',
             'body' => 'required'
         ];
 
@@ -109,6 +111,13 @@ class DashboardPostController extends Controller
         }
 
         $validatedData = $request->validate($rules);
+
+        if( $request->file('image') ){
+            if($request->oldImage) {
+                Storage::delete($request->oldImage);
+            }
+            $validatedData['image'] = $request->file('image')->store('post-images');
+        }
 
         $validatedData['user_id'] = auth()->user()->id;
         $validatedData['excerpt'] = Str::limit(strip_tags($request->body), 200, '...');
@@ -127,6 +136,12 @@ class DashboardPostController extends Controller
      */
     public function destroy(Post $post)
     {
+        //delete dulu file nya
+        if($post->image) {
+            Storage::delete($post->image);
+        }
+
+        //delete di table
         Post::destroy($post->id);
         
         return redirect('/dashboard/posts')->with('success', 'Post Has been Deleted!');
